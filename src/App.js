504a8720381756144a04d29a556a4d2dd813b987
Your Name <you@example.com>
2024-03-10 16:34:46 -0300

    final

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import './App.css';
 import React from "react";
 //Config router
 import {BrowserRouter, Routes, Route} from "react-router-dom";
+//Componentes
+import Navbar from "./components/Navbar";
+import SearchForm from "./components/SearchForm";
 //Pages
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Navbar from "./components/Navbar";
 import Products from "./pages/Products";
 import Info from "./pages/Info";
 import NotFound from "./pages/NotFound";
+import Search from "./pages/Search";
+
 
 
 function App() {
@@ -17,11 +21,13 @@ function App() {
       <h1>React Router</h1>
     <BrowserRouter>
     <Navbar />
+    <SearchForm />
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/about" element={<About />}/>
         <Route path="/products/:id/info" element={<Info />}/>  
         <Route path="/products/:id" element={<Products />}/>
+        <Route path='/search' element={<Search />} />
         <Route path="*" element={<NotFound />}/>
       </Routes>
     </BrowserRouter>
